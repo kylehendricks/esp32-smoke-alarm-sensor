fd3e23e4ad5b179ecf49c52589433a9dc765be23
Kyle Hendricks <kyle@hendricks.nu>
2018-03-07 22:49:31 -0500

    Get everything working

--- main/main.c
+++ main/main.c
@@ -13,11 +13,22 @@
 
 #include "mqtt_client.h"
 
+#define SMOKE_ALARM_READ_INTERVAL_MS 100
+
 static const int WIFI_CONNECTED_BIT = BIT0;
+static const int MQTT_CONNECTED_BIT = BIT1;
 static const char *TAG = "smoke_alarm";
 
 static EventGroupHandle_t connection_event_group;
 static esp_mqtt_client_handle_t mqtt_client = NULL;
+
+enum LastPublishedState {
+    LASTPUBLISHEDSTATE_UNPUBLISHED,
+    LASTPUBLISHEDSTATE_OFF,
+    LASTPUBLISHEDSTATE_ON
+};
+
+static enum LastPublishedState last_published_state = LASTPUBLISHEDSTATE_UNPUBLISHED;
 
 static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
@@ -43,39 +54,22 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
     return ESP_OK;
 }
 
-static void publish_to_smoke_alarm_topic(const char *msg, int len) {
-    esp_mqtt_client_publish(mqtt_client, CONFIG_MQTT_ALARM_TOPIC, msg, len, 1, 1);
-}
-
-static void task_handle_smoke_alarm_trigger(void *pvParameter) {
-    int level = (int) pvParameter;
-
-    if (level) {
-        publish_to_smoke_alarm_topic("on", 2);
-    } else {
-        publish_to_smoke_alarm_topic("off", 3);
-    }
-
-    vTaskDelete(NULL);
-}
-
-static void isr_smoke_alarm_triggered(void *args) {
-    int level = gpio_get_level((gpio_num_t) CONFIG_PIN_SMOKE_ALARM_RELAY);
-    xTaskCreate(&task_handle_smoke_alarm_trigger, "smoke_alarm_trigger", 8192, (void *const) level, 5, NULL);
+static int publish_to_smoke_alarm_topic(const char *msg, int len) {
+    return esp_mqtt_client_publish(mqtt_client, CONFIG_MQTT_ALARM_TOPIC, msg, len, 1, 1);
 }
 
 static esp_err_t mqtt_event_handler(esp_mqtt_event_handle_t event) {
     switch (event->event_id) {
         case MQTT_EVENT_CONNECTED:
             mqtt_client = event->client;
+            xEventGroupSetBits(connection_event_group, MQTT_CONNECTED_BIT);
             esp_mqtt_client_publish(mqtt_client, CONFIG_MQTT_LWT_TOPIC, "online", 6, 1, 1);
-
-            // Send a message with the current state of the alarm
-            isr_smoke_alarm_triggered(NULL);
 
             break;
         case MQTT_EVENT_DISCONNECTED:
+            xEventGroupClearBits(connection_event_group, MQTT_CONNECTED_BIT);
             mqtt_client = NULL;
+
             break;
         default:
             break;
@@ -91,9 +85,10 @@ static void init_mqtt() {
             .username = CONFIG_MQTT_USERNAME,
             .password = CONFIG_MQTT_PASSWORD,
             .port = CONFIG_MQTT_PORT,
-            .client_id = "salt_sensor",
+            .client_id = "smoke_alarm",
             .lwt_topic = CONFIG_MQTT_LWT_TOPIC,
             .lwt_msg = "offline",
+            .lwt_msg_len = 7,
             .lwt_qos = 1,
             .lwt_retain = 1,
             .keepalive = 60,
@@ -126,34 +121,51 @@ static void init_wifi()
     };
     ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
     ESP_ERROR_CHECK( esp_wifi_start() );
+    ESP_ERROR_CHECK(tcpip_adapter_set_hostname(TCPIP_ADAPTER_IF_STA, TAG));
     ESP_ERROR_CHECK( esp_wifi_connect() );
 }
 
-static void init_smoke_alarm_gpio() {
+static void init_gpio() {
     gpio_config_t smoke_alarm_pin_config = {
             .pin_bit_mask = (1 << CONFIG_PIN_SMOKE_ALARM_RELAY),
             .mode = GPIO_MODE_INPUT,
             .pull_up_en = GPIO_PULLUP_DISABLE,
             .pull_down_en = GPIO_PULLDOWN_ENABLE,
-            .intr_type = GPIO_INTR_ANYEDGE,
+            .intr_type = GPIO_INTR_DISABLE,
+    };
+
+    gpio_config_t wifi_led_pin_config = {
+            .pin_bit_mask = (1 << CONFIG_PIN_WIFI_STATUS_LED),
+            .mode = GPIO_MODE_OUTPUT,
     };
 
     ESP_ERROR_CHECK(gpio_config(&smoke_alarm_pin_config));
+    ESP_ERROR_CHECK(gpio_config(&wifi_led_pin_config));
+}
 
-    ESP_ERROR_CHECK(gpio_install_isr_service(0));
-    ESP_ERROR_CHECK(gpio_isr_handler_add((gpio_num_t) CONFIG_PIN_SMOKE_ALARM_RELAY, isr_smoke_alarm_triggered, NULL));
+static void task_monitor_smoke_alarm_relay(void *pvParameter) {
+    xEventGroupWaitBits(connection_event_group, MQTT_CONNECTED_BIT, 0, 1, portMAX_DELAY);
+
+    while(true) {
+        int level = gpio_get_level((gpio_num_t) CONFIG_PIN_SMOKE_ALARM_RELAY);
+        if (level && last_published_state != LASTPUBLISHEDSTATE_ON) {
+            publish_to_smoke_alarm_topic("on", 2);
+            last_published_state = LASTPUBLISHEDSTATE_ON;
+        } else if(!level && last_published_state != LASTPUBLISHEDSTATE_OFF) {
+            publish_to_smoke_alarm_topic("off", 3);
+            last_published_state = LASTPUBLISHEDSTATE_OFF;
+        }
+        vTaskDelay(pdMS_TO_TICKS(SMOKE_ALARM_READ_INTERVAL_MS));
+    }
 }
 
 void app_main()
 {
     nvs_flash_init();
 
-    gpio_pad_select_gpio(CONFIG_PIN_WIFI_STATUS_LED);
-    gpio_set_direction((gpio_num_t) CONFIG_PIN_WIFI_STATUS_LED, GPIO_MODE_OUTPUT);
-    gpio_set_level((gpio_num_t) CONFIG_PIN_WIFI_STATUS_LED, 0);
-
+    init_gpio();
     init_mqtt();
     init_wifi();
 
-    init_smoke_alarm_gpio();
+    xTaskCreate(&task_monitor_smoke_alarm_relay, "smoke_alarm_trigger", 8192, NULL, 5, NULL);
 }
